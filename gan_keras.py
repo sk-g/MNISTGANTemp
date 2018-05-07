9da59fa0fff6ddb24539ebcc99e20812008c0561
sk-g <sgouda@ucsc.edu>
2018-05-06 22:29:39 -0700

    more structural changes

--- gan_keras.py
+++ gan_keras.py
@@ -9,11 +9,12 @@ import collections
 import keras
 import tqdm
 import gc
+import keras
+import warnings
 
 import pandas as pd
 import numpy as np
-import keras
-import warnings
+import tensorflow as tf
 
 from keras.datasets import mnist
 from keras.models import Sequential
@@ -23,6 +24,9 @@ from keras.layers.convolutional import MaxPooling1D,MaxPooling2D
 from keras.optimizers import SGD
 from keras.preprocessing.image import ImageDataGenerator
 from keras.callbacks import ReduceLROnPlateau
+from keras.callbacks import TensorBoard
+#from keras.utils import plot_model
+from keras.utils.vis_utils import plot_model
 
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -62,10 +66,9 @@ def plotter(data,n_images = 36):
         plt.axis("off")
     plt.show()
 
-# Freeze weights in the discriminator for stacked training
 
 def make_trainable(net, val):
-
+    # Freeze weights in the discriminator for stacked training
     net.trainable = val
     for l in net.layers:
         l.trainable = val
@@ -81,14 +84,16 @@ def generator(data):
     opt = keras.optimizers.Adam(lr=0.01,
         decay=1e-5)
     # generative model
-    model = Sequential()
+    model = Sequential(name = "Generator_Network")
     
     
     
     nch = 256
-    model.add(Dense(nch*14*14,kernel_initializer='glorot_normal',activation = 'relu',input_shape = (100,)))
-    model.add(keras.layers.core.Reshape( [nch, 14, 14] ))
-    model.add(UpSampling2D((2,2),data_format = 'channels_first'))
+    model.add(Dense(nch*28*28,kernel_initializer='glorot_normal',activation = 'relu',input_shape = (100,)))
+    model.add(BatchNormalization())
+    model.add(keras.layers.core.Reshape( [nch, 28, 28] ))
+    model.add(Dropout(dropout_rate))
+
     
     model.add(Conv2D(nch//2,(3,3),
         data_format = 'channels_first',
@@ -96,12 +101,23 @@ def generator(data):
         padding = 'same',
         activation = 'relu'))
     
-    model.add(Conv2D(nch//4,(3,3),
+    model.add(Conv2D(nch//4,(1,1),
         kernel_initializer = 'glorot_normal',
         data_format = 'channels_first',
         padding = 'same',
         activation = 'relu'))
-    model.add(Conv2D(nch//4,(3,3),
+
+    model.add(Dropout(dropout_rate))
+
+    model.add(Conv2D(32,(5,5),
+        kernel_initializer = 'glorot_normal',
+        data_format = 'channels_first',
+        padding = 'same',
+        activation = 'relu'))
+
+
+    
+    model.add(Conv2D(1,(3,3),
         kernel_initializer = 'glorot_normal',
         data_format = 'channels_first',
         padding = 'same',
@@ -110,22 +126,11 @@ def generator(data):
     model.add(MaxPooling2D(pool_size=(2, 2),
         data_format = 'channels_first',
         padding = 'same'))
-    model.add(UpSampling2D((2,2),data_format = 'channels_first'))
-    model.add(Conv2D(1,(1,1),
-        kernel_initializer = 'glorot_normal',
-        data_format = 'channels_first',
-        padding = 'same',
-        activation = 'relu'))
     
-    model.add(keras.layers.LeakyReLU(alpha=0.3))
-    model.add(UpSampling2D((2,2),data_format = 'channels_first'))
-    model.add(MaxPooling2D(pool_size=(2,2),
-        data_format = 'channels_first',
-        padding = 'same'))
-        
-    model.add(BatchNormalization())
+    model.add(UpSampling2D((2,2),data_format = 'channels_first'))        
     model.compile(loss='binary_crossentropy', optimizer=opt)
-    #model.summary()
+    model.summary()
+    plot_model(model, show_shapes = True, to_file = "generator.png")
     return model
 
 def discriminator(data):
@@ -134,11 +139,12 @@ def discriminator(data):
     X_test = data['test']
     shp = X_train.shape[1:]
     dropout_rate = 0.25
-    dopt = keras.optimizers.Adam(lr=0.009,
-        decay=1e-5)
+    dopt = keras.optimizers.Adam(lr=0.0009,
+        decay = 1e-2)
     # Build Discriminative model ...
     #d_input = keras.layers.Input(shape=shp)
-    discriminator = Sequential()
+    discriminator = Sequential(name = "Discriminator_Network")
+    
     discriminator.add(Conv2D(256, (5, 5),
         strides =(2, 2), 
         padding = 'same',
@@ -146,20 +152,32 @@ def discriminator(data):
         data_format = "channels_first"))
     discriminator.add(keras.layers.LeakyReLU(0.2))
     discriminator.add(Dropout(dropout_rate))
-    discriminator.add(Conv2D(512, (3, 3),
+    
+    discriminator.add(Conv2D(256, (3, 3),
         data_format="channels_first",
-        strides=(2, 2),
-        activation="relu", 
+        strides=(2, 2), 
         padding="same"))
     discriminator.add(keras.layers.LeakyReLU(0.2))
     discriminator.add(Dropout(dropout_rate))
+    
+    discriminator.add(MaxPooling2D(pool_size=(2,2),
+        data_format = 'channels_first',
+        padding = 'same'))
+
+    discriminator.add(Conv2D(512, (1, 1),
+        data_format="channels_first", 
+        padding="same"))
+    discriminator.add(keras.layers.LeakyReLU(0.2))
+    discriminator.add(Dropout(dropout_rate))    
+    
     discriminator.add(Flatten())
     discriminator.add(Dense(256,activation = 'relu'))
+    discriminator.add(BatchNormalization())
     discriminator.add(Dropout(dropout_rate))
     discriminator.add(Dense(2,activation='softmax'))
     discriminator.compile(loss='categorical_crossentropy', optimizer=dopt)
-    #discriminator.summary()
-
+    discriminator.summary()
+    plot_model(discriminator, show_shapes = True, to_file = "discriminator.png")
     discriminator = make_trainable(discriminator, False)
     return discriminator
 
@@ -167,16 +185,18 @@ def GAN(data,d,g):
     
     print("\nBuilding the GAN Network")
 
-    opt = keras.optimizers.Adam(lr=0.01,
-        decay=1e-5)
+    opt = keras.optimizers.Adam(lr=0.001,
+        decay=1e-3)
 
     
     # creating the GAN network
-    gan = Sequential()
+    gan = Sequential(name = "GAN_Network")
     gan.add(g)
     gan.add(d)
     gan.compile(loss = 'categorical_crossentropy',optimizer = opt)
-    #gan.summary()
+    graph = plot_model(gan, show_shapes = True, to_file = "gan.png")
+
+    gan.summary()
     return gan
 def plot_temp(arr,name = None):
     print(name)#"real MNIST images")
@@ -191,8 +211,8 @@ def plot_temp(arr,name = None):
     plt.show()
 
 def plot_loss(losses):
-    IPython.display.clear_output(wait=True)
-    IPython.display.display(plt.gcf())
+    #IPython.display.clear_output(wait=True)
+    #IPython.display.display(plt.gcf())
     plt.figure(figsize=(10,8))
     plt.plot(losses["d"], label='discriminitive loss')
     plt.plot(losses["g"], label='generative loss')
@@ -212,7 +232,8 @@ def plot_gen(n_epoch = None, epochs = None,g = None,n_ex=9,dim=(3,3), figsize=(8
         plt.axis('off')
     plt.tight_layout()
     plt.savefig(os.getcwd()+os.sep+'results'+os.sep+"{0}_epochs_{1}.png".format(epochs,n_epoch))
-    plt.show()
+    #plt.show()
+    plt.close()
 def train_for_n(data,nb_epoch=5000, plt_frq=25,BATCH_SIZE=32):
 
     X_train, X_test = data['train'],data['test']
@@ -220,8 +241,7 @@ def train_for_n(data,nb_epoch=5000, plt_frq=25,BATCH_SIZE=32):
     g = generator(data)#model
     d = discriminator(data)#model
     gan = GAN(data,d,g)
-
-    
+  
     
     # generate some images
     ntrain = 12000
@@ -242,7 +262,8 @@ def train_for_n(data,nb_epoch=5000, plt_frq=25,BATCH_SIZE=32):
     y[n:,0] = 1
     ### getting discriminator performance upto
     ### this point
-    d.fit(X,y, verbose = 2,nb_epoch=2, batch_size=256)
+    print("Pre-training the discriminator\n")
+    d.fit(X,y, verbose = 2,nb_epoch = 2, batch_size = 128)
     y_hat = d.predict(X)
     y_hat_idx = np.argmax(y_hat,axis=1)
     y_idx = np.argmax(y,axis=1)
@@ -255,12 +276,14 @@ def train_for_n(data,nb_epoch=5000, plt_frq=25,BATCH_SIZE=32):
     # plot_temp(generated_images,name = "generated images")
     del acc,n_rig,n_tot,diff,y_hat,y_idx,y_hat_idx
     gc.collect()
+    
+    # unfreeze discriminator
     make_trainable(d,True)
-    for e in tqdm.tqdm(range(1, nb_epoch)):
-        
+
+    for e in tqdm.tqdm(range(1, nb_epoch+1)):
         # Make generative images
         image_batch = X_train[np.random.randint(0,X_train.shape[0],size=BATCH_SIZE),:,:,:]    
-        noise_gen = np.random.uniform(0,1,size=[BATCH_SIZE,100])
+        noise_gen = np.random.uniform(0,1,size=[BATCH_SIZE,100])*0.001
         generated_images = g.predict(noise_gen)
         
         # Train discriminator on generated images
@@ -280,16 +303,18 @@ def train_for_n(data,nb_epoch=5000, plt_frq=25,BATCH_SIZE=32):
         
         make_trainable(d,False)
         g_loss = gan.train_on_batch(noise_tr, y2 )
+        
         losses["g"].append(g_loss)
         
         if count%plt_frq == 0:
             # Updates plots
-            plot_loss(losses)
-            #plot_gen(g = g,epochs = e+1, n_epoch = nb_epoch)
+            #plot_loss(losses)
+            plot_gen(g = g,epochs = e+1, n_epoch = nb_epoch)
         count += 1
     return losses
 
 data = load_data()
-
-losses = train_for_n(data,nb_epoch=5000, plt_frq=5,BATCH_SIZE=32)
+#d = discriminator(data)
+#g = generator(data)
+losses = train_for_n(data, nb_epoch = 25000, plt_frq = 500, BATCH_SIZE = 256)
 plot_loss(losses)
